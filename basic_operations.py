39b57d8619b794621b42e0429b745c02807c7b17
stephaniezedek <stephaniemeanscrown@gmail.com>
2025-05-27 12:34:34 -0700

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -4,7 +4,7 @@ number1 - number2
 number1 + number2
 number1 * number2
 
-print("Addition of", number1, "and", number2, "is",number1 + number2)
+print("Addition of", number1, "and", number2, "is", number1 + number2)
 print("Subtraction of", number1, "and", number2, "is", number1 - number2)
 print("Multiplication of", number1, "and", number2, "is", number1 * number2)
 
