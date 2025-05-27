9b5f48221066ea8a3d2daf819ba83726fe2a3245
stephaniezedek <stephaniemeanscrown@gmail.com>
2025-05-27 12:10:49 -0700

    Update basic_operations.py

--- basic_operations.py
+++ basic_operations.py
@@ -3,9 +3,9 @@ number2 = 5
 Difference = number1 - number2
 Sum = number1 + number2
 Product = number1*number2
-print ("Subtraction of number1 and number2 is", Difference)
-print ("Addition of number1 and number2 is", Sum)
-print ("Multiplication of number1 and number2 is", Product)
+print ("Subtraction of", number1, "and", number2, "is", Difference)
+print ("Addition of", number1, "and", number2, "is", Sum)
+print ("Multiplication of", number1, "and", number2, "is", Product)
 python3 basic_operations.py
 Addition of 10 and 5 is 15
 Subtraction of 10 and 5 is 5
