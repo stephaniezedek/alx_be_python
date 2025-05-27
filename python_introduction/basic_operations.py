1235b89ba2a29496df65b606d709a8f5f0f31904
stephaniezedek <stephaniemeanscrown@gmail.com>
2025-05-27 11:54:34 -0700

    Add files via upload

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+basic_operations.py
+number1 = 10 
+number2 = 5
+Difference = number1 - number2
+Sum = number1 + number2
+Product = number1*number2
+print ("Difference of number1 and number2 is", Difference)
+print ("Sum of number1 and number2 is", Sum)
+print ("Product of number1 and number2 is", Product)
