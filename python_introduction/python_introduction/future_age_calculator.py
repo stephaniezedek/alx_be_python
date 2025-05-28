8b6be356f668cbf75d57fa6ad057e922032dfa8b
stephaniezedek <stephaniemeanscrown@gmail.com>
2025-05-28 09:23:55 -0700

    Add files via upload

---
+++ python_introduction/python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+presentage = input ("How old are you?")
+number = int(presentage)
+#Calculate the age after 27 years
+a = 27
+age = int(presentage) + int(a)
+print ("In 2050, you will be", age, "years old.")
---
+++ python_introduction/python_introduction/hours_to_seconds.py
@@ -0,0 +1,3 @@
+hours = 2
+seconds = 3600 * 2
+print ("2 hours (is)", seconds,"seconds.")
